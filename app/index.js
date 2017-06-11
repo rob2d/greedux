7041dad9111236eb282c693df00ed33d8f963454
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2017-06-11 17:39:16 -0400

    use namespace "app" for yeoman generator and update npm 5 package-lock.json file

---
+++ app/index.js
@@ -0,0 +1,6 @@
+// our base generator file;
+// see Yeoman documentation for details:
+// http://yeoman.io/authoring/
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {};
