b9de6e1fd57fb8fc01cac5d134bad78193c7c8d3
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2017-06-11 21:32:28 -0400

    rename build-env folder to build-config, add yeoman generator behavior and changes to README

--- app/index.js
+++ app/index.js
@@ -1,6 +1,21 @@
 // our base generator file;
 // see Yeoman documentation for details:
 // http://yeoman.io/authoring/
+
+const GLOB_OPTS = { globOptions : true };
+
 var Generator = require('yeoman-generator');
 
-module.exports = class extends Generator {};
+module.exports = class extends Generator
+{
+    app ()
+    {
+        this.copy('./.gitignore', './.gitignore');
+        this.copy('./gulpfile.js', './gulpfile.js');
+        this.copy('./src/build-config/**/*.*', './src/build-config/**/*.*', GLOB_OPTS);
+        this.copy('./src/js/**/*.*', './src/js/**/*.*', GLOB_OPTS);
+        this.copy('./dist/src/index.html', './dist/src/index.html', GLOB_OPTS);
+        this.copy('./dist/src/css/**/*.*', './dist/src/css/**/*.*', GLOB_OPTS);
+        this.copy('./dist/fonts/fonts/**/*.*', './dist/src/fonts/**/*.*', GLOB_OPTS);
+    }
+};
