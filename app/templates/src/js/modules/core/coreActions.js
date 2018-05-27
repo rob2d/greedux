5c40bca646b49bf42c5e6253c0b8a1c80d394e28
Nicholas Yang <ny585@nyu.edu>
2018-05-27 15:54:56 +0700

    fixed import bugs

--- app/templates/src/js/modules/core/coreActions.js
+++ app/templates/src/js/modules/core/coreActions.js
@@ -1,6 +1,6 @@
 export const REFRESH_WINDOW_DIMENSIONS = 'core/REFRESH_WINDOW_DIMENSIONS';
 
 export const refreshWindowDimensions = ()=> ({
-    type : t.REFRESH_WINDOW_DIMENSIONS,
+    type : REFRESH_WINDOW_DIMENSIONS,
     payload : {}
 });
