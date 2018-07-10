c1e5646d51016008234b96ba753d09976fd3aae8
Jason Kao <jasonkao85@gmail.com>
2018-07-10 10:49:47 -0400

    fixed redux store import

--- app/templates/src/js/store.js
+++ app/templates/src/js/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from 'redux/lib/applyMiddleware'
+import { applyMiddleware, createStore } from 'redux'
 import thunk  from 'redux-thunk'
 import promise from 'redux-promise-middleware'
 import reducer from './reducers'
